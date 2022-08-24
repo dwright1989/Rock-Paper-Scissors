7dde288f785f63d3f477298db1e6ecb62ffa6564
Debbie Wright <debbiewright@live.co.uk>
2022-08-24 13:49:25 +0100

    Fix for corruption

--- script.js
+++ script.js
@@ -21,51 +21,75 @@ function getRandomInt(max){
     return Math.floor(Math.random() * max);
 }
 
+/*
+Check to see if anyone has a score of 5 yet
+*/
+function checkWinner(){ // #################### change to return winner
+    console.log("in the check winner function");
+    if(userScore == 5 || computerScore ==5){
+        return true;
+    }else{
+        return false;
+    }
+}
+
+
 
 /*
 Play a round of the Rock, Paper, Scissors Game
 Update the score accordingly (Paper beats Rock, Rock beats Scissors, Scissors beat Paper)
 */
-function playRound (playerSelection, computerSelection){
-    playerSelection = playerSelection.toUpperCase();
-    computerSelection = computerSelection.toUpperCase();
+function playRound (playerSelection){
+
+    let computerSelection = computerPlay();
+
+    let theResultText = document.getElementById("whoWon");
     
     // find out the winner and update the score
     switch(playerSelection){
         
-        case "ROCK":
-            if (computerSelection=="ROCK"){
+        case "Rock":
+            if (computerSelection=="Rock"){
+                theResultText.innerHTML = "Computer also chose Rock.  It's a draw. NEXT ROUND!";
                 console.log("We chose the same! NEXT ROUND!");
-            }else if(computerSelection=="PAPER"){
+            }else if(computerSelection=="Paper"){
+                theResultText.innerHTML = "Computer chose Paper.  Paper beats Rock. Computer Wins! NEXT ROUND!";
                 console.log("The computer wins! NEXT ROUND!");
                 computerScore++;
-            }else if(computerSelection=="SCISSORS"){
+            }else if(computerSelection=="Scissors"){
+                theResultText.innerHTML = "Computer chose Scissors.  Rock beats Scissors. You Win! NEXT ROUND!";
                 console.log("You win! NEXT ROUND!");
                 userScore++;
             }else{
                 console.log("Hmmm... Something went wrong...");
             }
             break;
-        case "PAPER":
-            if (computerSelection=="PAPER"){
+        case "Paper":
+            if (computerSelection=="Paper"){
+                theResultText.innerHTML = "Computer also chose Paper.  It's a draw. NEXT ROUND!";
                 console.log("We chose the same! NEXT ROUND!");
-            }else if(computerSelection=="SCISSORS"){
+            }else if(computerSelection=="Scissors"){
+                theResultText.innerHTML = "Computer chose Scissors.  Scissors beats Paper. Computer Wins! NEXT ROUND!";
                 console.log("The computer wins! NEXT ROUND!");
                 computerScore++;
-            }else if(computerSelection=="ROCK"){
+            }else if(computerSelection=="Rock"){
+                theResultText.innerHTML = "Computer chose Rock.  Paper beats Rock. You Win! NEXT ROUND!";
                 console.log("You win! NEXT ROUND!");
                 userScore++;
             }else{
                 console.log("Hmmm... Something went wrong...");
             }
             break;
-        case "SCISSORS":
-            if (computerSelection=="SCISSORS"){
+        case "Scissors":
+            if (computerSelection=="Scissors"){
+                theResultText.innerHTML = "Computer also chose Scissors.  It's a draw. NEXT ROUND!";
                 console.log("We chose the same! NEXT ROUND!");
-            }else if(computerSelection=="ROCK"){
+            }else if(computerSelection=="Rock"){
+                theResultText.innerHTML = "Computer chose Rock.  Rock beats Scissors. Computer Wins! NEXT ROUND!";
                 console.log("The computer wins! NEXT ROUND!");
                 computerScore++;
-            }else if(computerSelection=="PAPER"){
+            }else if(computerSelection=="Paper"){
+                theResultText.innerHTML = "Computer chose Paper.  Scissors beats Paper. You Win! NEXT ROUND!";
                 console.log("You win! NEXT ROUND!");
                 userScore++;
             }else{
@@ -73,6 +97,20 @@ function playRound (playerSelection, computerSelection){
             }
             break;
 
+    }
+
+    console.log("user score: " + userScore);
+    let userScoreElement = document.getElementById("playerScore");
+    userScoreElement.innerText = userScore;
+    console.log("computer score: " + computerScore);
+    let computerScoreElement = document.getElementById("computerScore");
+    computerScoreElement.innerText = computerScore;
+
+    /* Check to see if anyone has won the game */
+    let hasAnyoneWon = checkWinner();
+    console.log(hasAnyoneWon);
+    if(hasAnyoneWon){
+        alert("Game Over"); // to be completed to wipe score and display
     }
 }
 
@@ -82,27 +120,26 @@ Let's start the game!
 function game(){
    
     // loop 5 times (the number of times we play the game)
-    for(let i=0; i<5; i++){
+    //for(let i=0; i<5; i++){
         // get the user input
-        let userInput = prompt("What's your choice? Rock, Paper or Scissors?");
-        // get the computer's choice
-        let computerInput = computerPlay();
-        // display the choices before calculating the winner
-        console.log("Player chose: " + userInput + " Computer chose: " + computerInput);
-        playRound (userInput, computerInput);
-    }
-    console.log("The results are in...");
-    console.log("Player score: " + userScore + " Computer Score: " + computerScore);
-    if(userScore>computerScore){
-        console.log("The player wins! Well done");
-    }else if(computerScore>userScore){
-        console.log("The computer wins! Better luck next time");
-    }else{
-        console.log("Hmm... any error!");
-    }
+        //let userInput = prompt("What's your choice? Rock, Paper or Scissors?");
 
-    
+        
+            console.log("in the game function");
+            const playerButtons = document.getElementsByClassName("playerButton");
+            console.log(playerButtons);
+            console.log(playerButtons.length);
+            for (let i=0; i<playerButtons.length; i++) {
+                console.log("in the for function");
+                playerButtons[i].addEventListener('click', function(){
+                    console.log(this.id);
+                    playRound (this.id);
+                
+                }) ; 
+            }     
+        
 }
+        
 
  // set global score variables
  let userScore = 0;
